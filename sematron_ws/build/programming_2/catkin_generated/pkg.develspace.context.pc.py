54bb9a4a8eed8651b2921ff4893e3b9237c0e5cc
Victor T. Noppeney <victortn21@hotmail.com>
2017-05-21 16:21:37 -0300

    Unstaged build & devel

--- sematron_ws/build/programming_2/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "programming_2"
-PROJECT_SPACE_DIR = "/home/victor/sematron_ros/sematron_ws/devel"
-PROJECT_VERSION = "0.0.0"
--- sematron_ws/build/programming_3/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/victor/sematron_ros/sematron_ws/install/include".split(';') if "/home/victor/sematron_ros/sematron_ws/install/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lprogramming_3".split(';') if "-lprogramming_3" != "" else []
-PROJECT_NAME = "programming_3"
-PROJECT_SPACE_DIR = "/home/victor/sematron_ros/sematron_ws/install"
-PROJECT_VERSION = "0.0.0"
