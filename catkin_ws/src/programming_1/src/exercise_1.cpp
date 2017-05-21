7d0af0f7260926caea0035f337a5386ad97b82ec
Victor T. Noppeney <victortn21@hotmail.com>
2017-05-21 14:26:21 -0300

    Programming 1 done

--- catkin_ws/src/programming_1/src/exercise_1.cpp
+++ catkin_ws/src/programming_1/src/exercise_1.cpp
@@ -1,32 +1,26 @@
 #include <stdio.h>
 
-
 /** include ros libraries  ************/
 #include <std_msgs/String.h>
 #include <ros/ros.h>
 
-
-
 int main(int argc, char **argv)
 {
-
-//***  First: Initialize ROS giving it argc, argv and a Name ***
-
+    //***  First: initialize ROS giving it argc, argv and a Name ***
 
 
-//*** Second: Create a Node  ***
+    //*** Second: create a Node  ***
 
 
+    //*** Third: create a Publisher inside that Node, the publisher is defined by a type, a name and the size of the buffer
 
-//*** Third: Create a Publisher inside that Node, the publisher is defined by a type, a name and the size of the buffer
+
+    //*** NB: remember of creating a ros::Rate here
 
 
-//*** NB: Remember of creating a ros::Rate here
-
-    while( ros::ok() ){
-
-        //*** Fourth: Create a message and put in it some content ***
-
+    while( ros::ok() )
+    {
+        //*** Fourth: create a message and put in it some content ***
 
 
         //*** Fifth: publish the message
@@ -35,8 +29,8 @@ int main(int argc, char **argv)
         //*** Sixth: make a ROS_INFO
 
 
-        //*** Last: remember of spinning and sleep for a while
+        //*** Last: remember of spinning and sleeping for a while
+
 
     }
-
 }
--- catkin_ws/src/programming_1/src/exercise_2_done.cpp
+++ catkin_ws/src/programming_1/src/exercise_2_done.cpp
@@ -6,21 +6,22 @@
 //*** Create one callback function with ROS_INFO
 void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
+  ROS_INFO("I heard: [%s]", msg->data.c_str());
 }
 
 int main(int argc, char **argv)
 {
-  //***  First: initialize ROS giving it argc, argv and a Name ***
+  //***  First: Initialize ROS giving it argc, argv and a Name ***
+  ros::init(argc, argv, "listener");
 
-
-  //*** Second: create a Node  ***
-
+  //*** Second: Create a Node  ***
+  ros::NodeHandle n;
 
   //*** Third: create a Subscriber inside that Node, it is defined by a topic name, the size of the buffer  and the CallBack function
-
+  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
 
   //*** Fourth: remember to spin the code
-
+  ros::spin();
 
   return 0;
 }
