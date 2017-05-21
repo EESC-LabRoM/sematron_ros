e146b777ad1d5686f4699cb08852715f6c71c953
Victor T. Noppeney <victortn21@hotmail.com>
2017-05-21 10:48:22 -0300

    Deleted build and devel folders

--- catkin_ws/build/programming_1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/victor/sematron_ros/catkin_ws/src/programming_1
-# Build directory: /home/victor/sematron_ros/catkin_ws/build/programming_1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
